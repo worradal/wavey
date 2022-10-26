8ed4eeef566c856e9f8e3b61db860ab0a7849192
himaghna <himaghna@udel.edu>
2022-10-26 10:36:10 -0700

    Add ARPLS class

--- baseline_correction.py
+++ baseline_correction.py
@@ -1,35 +1,51 @@
 """Baseline correction"""
 import numpy as np
+from numpy.linalg import norm
+from scipy import sparse
+from scipy.sparse import linalg
 
-from wavey.spectrum import Spectrum
+from .exceptions import NotConvergedError
 
-class IAsLS:
-    def __init__(self, p, lambda_, lambda_1) -> None:
-        self.p = p
-        self.lambda_ = lambda_
-        self.lambda_1 = lambda_1
+class ARPLS:
+    """Implements the Asymmetrically reweighted penalized least square method from [1]
     
-    def get_baseline(self, spectrum: Spectrum) -> np.ndarray:
-        y0 = spectrum.intensities
-        z0 = self._get_second_order_fit(y0)
+    References
+    [1]: Baek, S.-J., Park, A., Ahn, Y.-J. & Choo, J.
+        Baseline correction using asymmetrically reweighted penalized least squares smoothing. 
+        Analyst 140, 250–257 (2014).
+  
+    """
+    def __init__(self, lambda_: float) -> None:
+        self.lambda_ = lambda_
+    
+    def get_baseline(self, y: np.ndarray, stop_ratio: float=1e-6, max_iters: int=10, full_output=False)-> np.ndarray:
+        L = len(y)
+        diag = np.ones(L - 2)
+        D = sparse.spdiags([diag, -2*diag, diag], [0, -1, -2], L, L - 2)
+        H = self.lambda_ * D.dot(D.T)  # The transposes are flipped w.r.t the Algorithm on pg. 252
+        w = np.ones(L)
+        W = sparse.spdiags(w, 0, L, L)
 
-        def _get_residuals(y, z):
-            return y - z
+        current_ratio = 1
+        num_iters = 0
+        while current_ratio > stop_ratio:
+            z = linalg.spsolve(W + H, W * y)
+            d = y - z
+            dn = d[d < 0]
+            m = np.mean(dn)
+            s = np.std(dn)
+            w_new = 1 / (1 + np.exp(2 * (d - (2*s - m))/s))
+            current_ratio = norm(w_new - w) / norm(w)
+            w = w_new
+            W.setdiag(w)  # Do not create a new matrix, just update diagonal values
+            
+            num_iters += 1
+            if num_iters > max_iters:
+                raise NotConvergedError('Maximum number of iterations exceeded')
 
-        def _update_w(residuals) -> np.ndarray:
-            w = []
-            for resid in residuals:
-                if resid < 0:
-                    w.append(1-self.p)
-                else:
-                    w.append(self.p)
-            return np.array(w)
-        
-        curr_w = _update_w(_get_residuals(y0, z0))
-        zi = _solve_zi(w, y0) #####
-        new_w = _update_w(_get_residuals(y0, zi))
-        
-
-        
-
-
+        if full_output:
+            info = {'num_iters': num_iters, 'final_ratio': current_ratio}
+            return z, d, info
+        else:
+            return z
+    
